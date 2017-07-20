74ac8f3b9452d7bce722a5692566c61181a36fd3
boomsya <boomsya@gmail.com>
2017-07-20 12:47:38 +0300

    1.0.8
    getStepFragment

--- smartwizard/src/main/java/com/boomsya/smartwizard/Page.java
+++ smartwizard/src/main/java/com/boomsya/smartwizard/Page.java
@@ -63,6 +63,8 @@ public abstract class Page implements PageTreeNode {
 
     public abstract void getDataItems(ArrayList<DataItem> dest);
 
+    public abstract Fragment getStepFragment();
+
     public boolean isCompleted() {
         return true;
     }
